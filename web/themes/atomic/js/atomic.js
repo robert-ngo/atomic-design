a0d6e4811257908fd5b7041f489f050c214a4e49
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-07-06 12:56:08 -0400

    Add and set theme atomic

---
+++ web/themes/atomic/js/atomic.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * Atomic behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.atomic = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
